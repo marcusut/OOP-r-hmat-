45c5ab9f4bc2a9db3bf0b721280ad5acd0ce575f
Heidi Hein <146388144+paradoksaalsus@users.noreply.github.com>
2024-04-04 17:19:32 +0300

    Update Mäng.java

--- src/Mäng.java
+++ src/Mäng.java
@@ -83,42 +83,6 @@ class Mäng {
         }
     }
 
-    // kui sõna on õieti ära arvatud, siis lisame selle arvatudSõnad listi
-    boolean arvaSõna(String sõna) {
-        sõna = sõna.toLowerCase();
-        if (lemmad.sõnaEksisteerib(sõna)) {
-            arvatudSõnad.add(lemmad.getSõnaOriginaalVorm(sõna));
-            lemmad.eemaldaSõna(sõna);
-            return true;
-        }
-        return false;
-    }
-
-    boolean mängijaArvab(String sõna, String tähed) {
-        if (sõna.equalsIgnoreCase("quit")) {
-            System.exit(0);
-        }
-        if (sõna.equalsIgnoreCase("menüüsse")) {
-            lõpetaMäng();
-            return false;
-        }
-        if (sõna.equalsIgnoreCase("+")) {
-            this.tähed += genereeriÜksTäht();
-            return false;
-            }
-        if (!sõnaOnMoodustatudPakutudTähtedest(sõna, tähed)) {
-            System.out.println("Sõna ei saa moodustada pakutud tähtedest. Proovi uuesti.");
-            return false;
-        }
-        if (arvaSõna(sõna)) {
-            System.out.println("Õige sõna!");
-            return true;
-        } else {
-            System.out.println("Vale sõna. Proovi uuesti.");
-            return false;
-        }
-    }
-
     boolean mängAktiivne = true;
 
     String genereeriTähed() {
@@ -147,4 +111,3 @@ class Mäng {
     }
 
 }
-
