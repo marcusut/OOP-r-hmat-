1bed114043155ca65e633713d32008f3f51ea404
Heidi Hein <146388144+paradoksaalsus@users.noreply.github.com>
2024-04-04 16:33:15 +0300

    Update Mäng.java

--- src/Mäng.java
+++ src/Mäng.java
@@ -53,23 +53,6 @@ class Mäng {
     }
 
 
-
-    boolean sõnaOnMoodustatudPakutudTähtedest(String sõna, String tähed) {
-        Map<Character, Integer> tähedKaart = new HashMap<>();
-        for (char c : tähed.toCharArray()) {
-            tähedKaart.put(c, tähedKaart.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : sõna.toCharArray()) {
-            if (!tähedKaart.containsKey(c) || tähedKaart.get(c) == 0) {
-                return false;
-            }
-            tähedKaart.put(c, tähedKaart.get(c) - 1);
-        }
-        return true;
-    }
-
-
     static void alustaMäng() {
         while (true) {
             Mäng mäng = valiMängutüüp();
