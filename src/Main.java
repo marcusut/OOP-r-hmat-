3dcbe3a11533612b464d7a0016afa66e72d7c585
Heidi Hein <146388144+paradoksaalsus@users.noreply.github.com>
2024-04-04 16:35:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ class Main {
 
     public static void main(String[] args) {
         System.out.println("Tere tulemast meie mängu.");
-        System.out.println("Eesmärk on teile antud tähtedest moodustada eestikeelseid sõnu.");
+        System.out.println("Mängu eesmärk on olemasolevatest mängu poolt genereeritud tähtedest moodustada eestikeelseid sõnu.");
         Mäng.alustaMäng();
     }
 }
